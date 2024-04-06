24b5f890204b9ec1e1d383ed7b88eb13e573c3c9
techatikin <nikitakhabya03@gmail.com>
2024-04-06 15:56:29 +0530

    fix: mix bug in recommend and follow service

--- app/(main)/_components/sidebar/Recommended.tsx
+++ app/(main)/_components/sidebar/Recommended.tsx
@@ -1,12 +1,12 @@
 "use client"
 
 import { useSidebar } from '@/store/use-sidebar'
-import { Stream, User } from '@prisma/client'
+import { User } from '@prisma/client'
 import UserItem, { UserItemSkeleton } from './UserItem'
 
 interface RecommendedProps {
   data: (User & {
-    stream: Stream | null
+    stream: { isLive: boolean } | null
   })[]
 }
 
