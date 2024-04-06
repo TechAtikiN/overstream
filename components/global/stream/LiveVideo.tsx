d5a2533a7f3e951f77439533630ca18571be24fa
techatikin <nikitakhabya03@gmail.com>
2024-04-06 22:56:09 +0530

    feat: volume controls on stream video

--- components/global/stream/LiveVideo.tsx
+++ components/global/stream/LiveVideo.tsx
@@ -1,10 +1,11 @@
 "use client"
 
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Participant, Track } from "livekit-client"
 import { useTracks } from "@livekit/components-react"
-import FullScreenControl from "./FullScreenControl"
 import { useEventListener } from "usehooks-ts"
+import FullScreenControl from "./FullScreenControl"
+import VolumeControl from "./VolumeControl"
 
 interface LiveVideoProps {
   participant: Participant
@@ -12,8 +13,31 @@ interface LiveVideoProps {
 
 export default function LiveVideo({ participant }: LiveVideoProps) {
   const [isFullScreen, setIsFullScreen] = useState(false)
+  const [volume, setVolume] = useState(0)
   const videoRef = useRef<HTMLVideoElement>(null)
   const wrapperRef = useRef<HTMLDivElement>(null)
+
+  const onVolumeChange = (value: number) => {
+    setVolume(+value)
+    if (videoRef?.current) {
+      videoRef.current.muted = value === 0
+      videoRef.current.volume = +value * 0.01
+    }
+  }
+
+  const toggleMute = () => {
+    const isMuted = volume === 0
+    setVolume(isMuted ? 50 : 0)
+
+    if (videoRef?.current) {
+      videoRef.current.muted = !isMuted
+      videoRef.current.volume = isMuted ? 0.5 : 0
+    }
+  }
+
+  useEffect(() => {
+    onVolumeChange(0)
+  }, [])
 
   const toggleFullScreen = () => {
     if (isFullScreen) {
@@ -53,6 +77,11 @@ export default function LiveVideo({ participant }: LiveVideoProps) {
           className="absolute bottom-0 flex h-14 w-full items-center justify-between
           bg-gradient-to-r from-neutral-900 px-4"
         >
+          <VolumeControl
+            onChange={onVolumeChange}
+            value={volume}
+            onToggle={toggleMute}
+          />
           <FullScreenControl
             isFullScreen={isFullScreen}
             onToggle={toggleFullScreen}
---
+++ components/global/stream/VolumeControl.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { Volume1, Volume2, VolumeX } from "lucide-react"
+import { Slider } from "@/components/ui/slider"
+import Hint from "../Hint"
+
+interface VolumeControlProps {
+  onToggle: () => void
+  onChange: (value: number) => void
+  value: number
+}
+
+export default function VolumeControl({ onToggle, onChange, value }: VolumeControlProps) {
+  const isMuted = value === 0
+  const isAboveHalf = value > 50
+
+  let Icon = Volume1
+
+  if (isMuted) {
+    Icon = VolumeX
+  } else if (isAboveHalf) {
+    Icon = Volume2
+  }
+
+  const label = isMuted ? "Unmute" : "Mute";
+
+  const handleChange = (value: number[]) => {
+    onChange(value[0])
+  }
+
+  return (
+    <div>
+      <Hint label={label} asChild>
+        <button
+          onClick={onToggle}
+          className="text-white hover:bg-white/10 p-1.5 rounded-lg"
+        >
+          <Icon className="h-6 w-6" />
+        </button>
+      </Hint>
+      <Slider
+        className="w-[8rem] cursor-pointer"
+        onValueChange={handleChange}
+        value={[value]}
+        max={100}
+        step={1}
+      />
+    </div>
+  )
+}
